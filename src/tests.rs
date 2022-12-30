6e5a551c10d38d726e69e49515a643adc7dc24f1
maskedball <maskedball@pop-os.localdomain>
2022-12-30 14:54:08 +0300

    add new tests

--- src/tests.rs
+++ src/tests.rs
@@ -25,3 +25,27 @@ fn test_is_two_forms_correct() {
     // Test that the function returns Err for an empty input
     assert!(is_two_forms_correct("").is_err());
 }
+
+#[test]
+fn test_validate_correct() {
+    let verb = IrregularVerb::new("see".to_string(), "saw".to_string(), "seen".to_string());
+    let result = validate("saw".to_string(), &&verb, "seen".to_string());
+    assert!(result.0);
+    assert_eq!(result.1, "".to_string());
+}
+
+#[test]
+fn test_validate_incorrect_past() {
+    let verb = IrregularVerb::new("see".to_string(), "saw".to_string(), "seen".to_string());
+    let result = validate("see".to_string(), &&verb, "seen".to_string());
+    assert!(!result.0);
+    assert_eq!(result.1, "see - saw - seen".to_string());
+}
+
+#[test]
+fn test_validate_incorrect_past_participle() {
+    let verb = IrregularVerb::new("see".to_string(), "saw".to_string(), "seen".to_string());
+    let result = validate("saw".to_string(), &&verb, "see".to_string());
+    assert!(!result.0);
+    assert_eq!(result.1, "see - saw - seen".to_string());
+}
